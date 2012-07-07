94fb2596b480f4e14b571879a45ecb8c62c758b7
Tom Bujok <tom.bujok@reficio.org>
2012-07-07 14:45:28 +0200

    Adding license info

--- src/test/java/org/reficio/p2/utils/BundleUtilsTest.java
+++ src/test/java/org/reficio/p2/utils/BundleUtilsTest.java
@@ -1,4 +1,6 @@
 /**
+ * Copyright (c) 2012 centeractive ag. All Rights Reserved.
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.reficio.p2.utils;
 
 import aQute.lib.osgi.Jar;
